e498e15916fa2bd8d10330c20541267d30d81149
Bautista2049 <49379221@est.ort.edu.ar>
2025-03-17 10:41:28 -0300

    ej 1 finished

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -12,7 +12,15 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        edadUser = 66;
+        sueldoUser = 1500.00f;
+        domicilio = "Libertador 1918";
+        estaAprobado = true;
+
+        Debug.Log(edadUser);
+        Debug.Log(sueldoUser);
+        Debug.Log(domicilio);
+        Debug.Log(estaAprobado);
     }
 
     // Update is called once per frame
