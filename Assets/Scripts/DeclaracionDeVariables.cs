7e6f3510f3807aef13940dffaaff287dc646ab2a
Bautista2049 <49379221@est.ort.edu.ar>
2025-03-17 10:28:46 -0300

    Variables Creadas

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class DeclaracionDeVariables : MonoBehaviour
 {
+    int edadUser;
+    float sueldoUser;
+    string domicilio;
+    bool estaAprobado;
+    
     // Start is called before the first frame update
     void Start()
     {
