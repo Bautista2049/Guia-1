8ea726b2a1a35a18b1a3d5d7ba7676778428e70b
Bautista2049 <49379221@est.ort.edu.ar>
2025-03-31 10:05:36 -0300

    User data

---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    public string nombreUsuario;
+    public int edadUsuario;
+    public float alturaUsuario;
+    public bool esDonante;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombreUsuario + " tengo " + edadUsuario + " años " + " mido " + alturaUsuario + " y soy " + esDonante + " donante.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
