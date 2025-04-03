f3312e5e7cedaca3dd7684589f9dc764479c1ae4
Bautista2049 <49379221@est.ort.edu.ar>
2025-04-03 10:41:25 -0300

    cc

---
+++ Assets/parte 2 scripts/num10.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class num10 : MonoBehaviour
+{
+    bool estaAbierto;
+    public int horaActual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (horaActual > 24 || horaActual < 0)
+        {
+         Debug.Log("Ha ingresado una hora incorrecta");
+         return;
+        }
+        else if(horaActual >= 10 && horaActual < 18)
+        {
+            estaAbierto = true;
+        }
+        else
+        {
+            estaAbierto = false; 
+        }
+        Debug.Log("El estabelcimiento esta ")
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+    }
+}
