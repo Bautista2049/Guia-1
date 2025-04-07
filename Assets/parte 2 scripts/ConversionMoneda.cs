af4a8af0c2c2bb66da5e885150d45c7c64ad5dfc
Bautista2049 <49379221@est.ort.edu.ar>
2025-04-07 10:41:36 -0300

    ejercisios prueba

---
+++ Assets/parte 2 scripts/ConversionMoneda.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConversionMoneda : MonoBehaviour
+{
+    //1. Declaracion de variables
+    public float monto;
+    public string moneda;
+    float montoConvertido;
+    float montoMinimo = 1000;
+    float cotizacionMonedaElegida;
+    float cotizacionEuro = 1621.3f;
+    float cotizacionDolar = 1293.21f;
+    float cotizacionReal = 1336.7f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2. Ingreso y validacion de datos
+        if (monto < montoMinimo) 
+        {
+            Debug.Log("El monto mínimo es $1000");
+            return;
+        }
+
+        if (moneda == "E")
+        {
+            cotizacionMonedaElegida = cotizacionEuro;
+        }
+        else if (moneda == "D")
+        {
+            cotizacionMonedaElegida = cotizacionDolar;
+        }
+        else if (moneda == "R") 
+        {
+            cotizacionMonedaElegida = cotizacionReal;
+        }
+        else
+        {
+            Debug.Log("Opción de moneda extranjera no válida");
+            return;
+        }
+        //3. Procesamiento de datos
+
+        montoConvertido = monto / cotizacionMonedaElegida;
+
+        //4. Salida de datos
+        Debug.Log("La cotización de ese monto a pesos es" + montoConvertido);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/parte 2 scripts/DeQueOrientacionSos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comidaAnimalitos : MonoBehaviour
+{
+    public string animal;
+    public int alimento;
+    public int duraciónEstadía;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (animal == "G")
+        {
+            alimento = 300;
+        }
+        else if (animal == "PP")
+        {
+            alimento = 400;
+        }
+        else if (animal == "PG")
+        {
+            alimento = 700;
+        }
+        else
+        {
+            Debug.Log("El codigo de animal no existe, ingrese G, PP o PG");
+            return;
+
+        }
+    } 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+} 
